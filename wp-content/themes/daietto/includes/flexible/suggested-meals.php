c6ed3d1f296201d99f01b6f9d44200ebc615cc9b
Mikołaj Czubak <mikolaj.czubak@crafton.pl>
2021-07-06 17:43:19 +0200

    content side

--- wp-content/themes/daietto/includes/flexible/suggested-meals.php
+++ wp-content/themes/daietto/includes/flexible/suggested-meals.php
@@ -12,13 +12,20 @@
               <image class="suggestedMeals__mealPlateImage" src="<?= $image['url'] ?>"></image>
             </div>
             <h3 class="suggestedMeals__mealTitle"><?= $title ?></h3>
-            <a class="suggestedMeals__mealButton" href="<?= get_permalink($id) ?>">></a>
+            <a class="suggestedMeals__mealButton" href="<?= get_permalink($id) ?>"></a>
           </div>
 
         <?php endforeach; ?>
       </div>
       <div class="suggestedMeals__content">
-
+        <div class="suggestedMeals__contentInner">
+          <div class="suggestedMeals__line"></div>
+          <h1 class="suggestedMeals__header h">
+            <span class="suggestedMeals__headerHightlighted h--light"><?= $section['header']['highlighted'] ?></span>
+            <?= $section['header']['regular'] ?>
+          </h1>
+          <p class="suggestedMeals__description p"><?= $section['description'] ?></p>
+        </div>
       </div>
     </div>
   </div>
