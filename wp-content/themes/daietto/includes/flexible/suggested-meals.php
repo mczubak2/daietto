a550f0e446671b695c12c0cb8ed13fcee8c57baa
Mikołaj Czubak <mikolaj.czubak@crafton.pl>
2021-07-07 00:03:59 +0200

    outputting macros

--- wp-content/themes/daietto/includes/flexible/suggested-meals.php
+++ wp-content/themes/daietto/includes/flexible/suggested-meals.php
@@ -2,16 +2,33 @@
   <div class="container">
     <div class="suggestedMeals__inner">
       <div class="suggestedMeals__meals">
-        <?php foreach ($section['meals'] as $item): 
+        <?php foreach ($section['meals'] as $item):
+
           $id    = $item['meal']->ID;
           $title = $item['meal']->post_title;
           $image = get_field('image', $id);
           $macro = get_field('macronutrients', $id);?>
+
           <div class="suggestedMeals__meal">
             <div class="suggestedMeals__mealPlate">
               <image class="suggestedMeals__mealPlateImage" src="<?= $image['url'] ?>"></image>
             </div>
             <h3 class="suggestedMeals__mealTitle"><?= $title ?></h3>
+            <div class="suggestedMeals__macronutrients">
+              <?php foreach ($macro as $key => $item): ?>
+                <?php if ($key == 'calories'): ?>
+                  <div class="suggestedMeals__macronutrient">
+                    <p class="suggestedMeals__macronutrientValue suggestedMeals__macronutrientValue--red"><?= $item ?></p>
+                    <p class="suggestedMeals__macronutrientName"><?= $section['calories'] ?></p>
+                  </div>
+                <?php elseif ($key == 'fat'): ?>
+                  <div class="suggestedMeals__macronutrient">
+                    <p class="suggestedMeals__macronutrientValue"><?= $item . 'g' ?></p>
+                    <p class="suggestedMeals__macronutrientName"><?= $section['fat'] ?></p>
+                  </div>
+                <?php endif; ?>
+              <?php endforeach; ?>
+            </div>
             <a class="suggestedMeals__mealButton" href="<?= get_permalink($id) ?>"></a>
           </div>
 
