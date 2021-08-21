0170b9663edc3b14829280c5213e7cd76dcbb564
Mikołaj Czubak <mikolaj.czubak@crafton.pl>
2021-08-21 18:54:16 +0200

    404

---
+++ wp-content/themes/daietto/404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<main>
+  <?php 
+    get_template_part('includes/sections/pageNotFound');
+  ?>
+</main>
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/daietto/includes/sections/pageNotFound.php
@@ -0,0 +1,7 @@
+<section class="pageNotFound">
+  <div class="container">
+    <div class="pageNotFound__inner">
+      <span class="pageNotFound__text">404</span>
+    </div>
+  </div>
+</section>
