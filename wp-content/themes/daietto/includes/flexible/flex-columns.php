78d5486a3caa2ce11a365bc73d3f170dc3e78dbb
Mikołaj Czubak <mikolaj.czubak@crafton.pl>
2021-08-21 14:59:18 +0200

    contact 7 plugin

--- wp-content/themes/daietto/includes/flexible/flex-columns.php
+++ wp-content/themes/daietto/includes/flexible/flex-columns.php
@@ -1,11 +1,20 @@
 <section class="flexColumns">
   <ul class="flexColumns__list">
     <?php foreach ($section['columns'] as $item): ?>
-      <li class="flexColumns__item" style="background-image: url('<?= $item['image']['url'] ?>')">
-        <h4 class="flexColumns__header">
-          <?= $item['header'] ?>
-        </h4>
-        <div class="flexColumns__content">
+      <li 
+        class="flexColumns__item" 
+        style="background-image: url('<?= $item['image']['url'] ?>')"
+      >
+        <div class="flexColumns__header">
+          <span class="flexColumns__headerLine"></span>
+          <h4 class="flexColumns__headerTitle">
+            <?= $item['header'] ?>
+          </h4>
+        </div>
+        <div 
+          class="flexColumns__content" 
+          style="width: <?= (100 / (count($section['columns']) + 1)) * 2 ?>vw"
+        >
           <?= $item['content'] ?>
         </div>
       </li>
---
+++ wp-content/themes/daietto/includes/sections/contact.php
@@ -0,0 +1,17 @@
+<section class="contactForm">
+  <div class="container">
+    <label> Your name
+    [text* your-name] </label>
+
+    <label> Your email
+        [email* your-email] </label>
+
+    <label> Subject
+        [text* your-subject] </label>
+
+    <label> Your message (optional)
+        [textarea your-message] </label>
+
+    [submit "Submit"]
+  </div>
+</section>
